dd097d591f6cb031959463617ef5e2de1037ae8d
Philipp Meschenmoser <philipp.meschenmoser@uni.kn>
2017-11-25 17:21:45 +0100

    transitive triple

---
+++ src/nm03/Main.java
@@ -0,0 +1,31 @@
+package nm03;
+
+
+public class Main {
+    public static void main(String[] args){
+        int[][] adj =  {{0, 1, 1}, {1, 1, 1}, {0, 1, 0}};
+        System.out.println(getTransitiveCount(adj));
+    }
+
+    private static int getTransitiveCount(int[][] adj){
+        int count = 0;
+        int l = adj[0].length;
+        if (l<3) return count;
+        //we only need to iterate the triangle, since (v,u) cannot be part of a triple
+        for (int u= 0; u<l-2; u++){
+            for (int v= u+1; v<l-1; v++){
+                if (adj[u][v]== 1) { //(u,v) exists
+                    for (int w = v + 1; w < l; w++) {
+                        if (adj[u][w] == 1 && adj[v][w] == 1){ //(u,w) and (v,w) also exist
+                            count++;
+                            System.out.println(u +","+ v +","+""+w);
+                        }
+
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+}
