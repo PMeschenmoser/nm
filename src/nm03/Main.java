2c1e22e4699307fca02f9ba4bb93e3643f1cce0d
Philipp Meschenmoser <philipp.meschenmoser@uni.kn>
2017-11-25 17:50:53 +0100

    transitive triple

--- src/nm03/Main.java
+++ src/nm03/Main.java
@@ -11,16 +11,15 @@ public class Main {
         int count = 0;
         int l = adj[0].length;
         if (l<3) return count;
-        //we only need to iterate the triangle, since (v,u) cannot be part of a triple
+        //we only need to iterate the triangle/pyramid, since (v,u) cannot be part of a triple
         for (int u= 0; u<l-2; u++){
             for (int v= u+1; v<l-1; v++){
                 if (adj[u][v]== 1) { //(u,v) exists
                     for (int w = v + 1; w < l; w++) {
+                        System.out.println("bla4");
                         if (adj[u][w] == 1 && adj[v][w] == 1){ //(u,w) and (v,w) also exist
                             count++;
-                            System.out.println(u +","+ v +","+""+w);
                         }
-
                     }
                 }
             }
